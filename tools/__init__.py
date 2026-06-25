2da80fba4e866a0f2cc8a917fc38d5010f833577
Noamalonn <Noamalonn7@gmail.com>
2026-06-26 00:40:15 +0300

    add emergency_tool to TOOL_REGISTRY and ALL_TOOLS

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,8 +1,9 @@
-from tools.gdacs_tool     import get_gdacs_alerts,                TOOL_DEFINITION as GDACS_DEF
+﻿from tools.gdacs_tool     import get_gdacs_alerts,                TOOL_DEFINITION as GDACS_DEF
 from tools.usgs_tool      import get_earthquakes_global,           TOOL_DEFINITION as USGS_DEF
 from tools.nasa_tool      import get_nasa_events,                  TOOL_DEFINITION as NASA_DEF
 from tools.rag_tool       import search_historical_and_analyze,    TOOL_DEFINITION as RAG_DEF
 from tools.analytics_tool import query_disaster_statistics,        TOOL_DEFINITION as STATS_DEF
+from tools.emergency_tool import get_israel_emergency_contacts,    TOOL_DEFINITION as EMERGENCY_DEF
 
 TOOL_REGISTRY = {
     "get_gdacs_alerts":               get_gdacs_alerts,
@@ -10,6 +11,7 @@ TOOL_REGISTRY = {
     "get_nasa_events":                get_nasa_events,
     "search_historical_and_analyze":  search_historical_and_analyze,
     "query_disaster_statistics":      query_disaster_statistics,
+    "get_israel_emergency_contacts":  get_israel_emergency_contacts,
 }
 
-ALL_TOOLS = [GDACS_DEF, USGS_DEF, NASA_DEF, RAG_DEF, STATS_DEF]
+ALL_TOOLS = [GDACS_DEF, USGS_DEF, NASA_DEF, RAG_DEF, STATS_DEF, EMERGENCY_DEF]
